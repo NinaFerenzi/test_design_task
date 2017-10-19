482097c46dd900c67997a09505e784a8de178dd0
Rocket Scientist <32873673+SEMrushQA@users.noreply.github.com>
2017-10-20 00:18:28 +0300

    [PHP] Made the php logic more obvious.
    remove array_merge.

--- backend.php
+++ backend.php
@@ -1,13 +1,13 @@
 <?php
-$request = $_POST;
-$template = array(
-   	'admin' => 0,
-'disabled' => 0,
-'activated' => 0
-);
-$new_user = array_merge($request, $template);
-/* 😋 */ 
-$query = "INSERT INTO 'user' (email, password, name, phone, country, domain, admin, disabled, activated, registration_date) VALUES ($new_user["email"],$new_user["password"], $new_user["name"], , $new_user["phone"], $new_user["country"], $new_user["domain"], $new_user["admin"], $new_user["disabled"], $new_user["activated"], now());
+   $request = $_POST;
+   $template = array(
+            'admin' => 0,
+            'disabled' => 0,
+            'activated' => 0
+         );
+   $new_user = $request + $template;
 
-$this->_db->query($query);
+   $query = "INSERT INTO 'user' (email, password, name, phone, country, domain, admin, disabled, activated, registration_date) VALUES ($new_user["email"],$new_user["password"], $new_user["name"], , $new_user["phone"], $new_user["country"], $new_user["domain"], $new_user["admin"], $new_user["disabled"], $new_user["activated"], now());
+
+   $this->_db->query($query);
 ?>
