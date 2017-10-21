0a17cef6633ad78ff32ce2c40bd66c1550d06bd1
Ivan Trechyokas <i.trechyokas@semrush.com>
2017-10-21 23:33:11 +0300

    [backend] Lost a quote

--- backend.php
+++ backend.php
@@ -7,7 +7,7 @@
          );
    $new_user = $request + $template;
 
-   $query = "INSERT INTO 'user' (email, password, name, phone, country, domain, admin, disabled, activated, registration_date) VALUES ($new_user["email"],$new_user["password"], $new_user["name"], $new_user["phone"], $new_user["country"], $new_user["domain"], $new_user["admin"], $new_user["disabled"], $new_user["activated"], now());
+   $query = "INSERT INTO 'user' (email, password, name, phone, country, domain, admin, disabled, activated, registration_date) VALUES ($new_user["email"],$new_user["password"], $new_user["name"], $new_user["phone"], $new_user["country"], $new_user["domain"], $new_user["admin"], $new_user["disabled"], $new_user["activated"], now());"
 
    $this->_db->query($query);
 ?>
